2b9ddbc32d2fccd55182678f270cefcad9188212
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-10 15:39:31 +0100

    Fixed issue of game ending on a possible tie. Added playRound function to run the game until 5 rounds is over prompting the user for 5 inputs.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 function getComputerChoice() {
 
     let computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -25,60 +19,62 @@ function getHumanChoice() {
     // if (humanChoice !== "Rock" || "Paper" ||  "Scissors") {
     //     alert("Must Choose Rock, Paper, or Scissors!!!")
     //     getHumanChoice()
-    
+
     // }
     return humanChoice
 }
 
 
-function playRound(humanSelection, computerSelection) {
-console.log(humanSelection)
-console.log(computerSelection)
-    if (humanSelection === computerSelection) {
-        console.log("It's a tie!")
-        round++
-    }
-    else if (humanSelection === "rock" && computerSelection === "scissors") {
-        console.log(`You win! ${humanSelection} beats ${computerSelection}!`)
-        humanScore++
-        round++
-        console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
-    }
-    else if (humanSelection === "paper" && computerSelection === "rock") {
-        console.log(`You win! ${humanSelection} beats ${computerSelection}!`)
-        humanScore++
-        round++
-        console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
-    }
-    else if (humanSelection === "scissors" && computerSelection === "paper") {
-        console.log(`You win! ${humanSelection} beats ${computerSelection}`)
-        humanScore++
-        round++
-        console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
-    }
-    else {
-        console.log(`You lose! ${computerSelection} beats ${humanSelection}!`)
-        computerScore++
-        round++
-        console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
-    }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-
-
-
-function playGame(){
+function playGame() {
 
     let humanScore = 0;
     let computerScore = 0;
     let round = 0;
+    let i = 0;
+
+    while (i < 5) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
 
 
+        function playRound(humanSelection, computerSelection) {
+            console.log(humanSelection)
+            console.log(computerSelection)
+            if (humanSelection === computerSelection) {
+                console.log("It's a tie!")
+            }
+            else if (humanSelection === "rock" && computerSelection === "scissors") {
+                console.log(`You win! ${humanSelection} beats ${computerSelection}!`)
+                humanScore++
+                round++
+                console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+            }
+            else if (humanSelection === "paper" && computerSelection === "rock") {
+                console.log(`You win! ${humanSelection} beats ${computerSelection}!`)
+                humanScore++
+                round++
+                console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+            }
+            else if (humanSelection === "scissors" && computerSelection === "paper") {
+                console.log(`You win! ${humanSelection} beats ${computerSelection}`)
+                humanScore++
+                round++
+                console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+            }
+            else {
+                console.log(`You lose! ${computerSelection} beats ${humanSelection}!`)
+                computerScore++
+                round++
+                console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+            }
+        }
+
+        playRound(humanSelection, computerSelection)
+        i++
+    }
 
 }
+
 
 
 
