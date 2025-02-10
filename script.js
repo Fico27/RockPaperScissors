7f2d4d2f54d04121ea42233c76f79c2654e1bd68
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-10 14:29:05 +0100

    Added in player choice and play found functions.

--- script.js
+++ script.js
@@ -1,15 +1,80 @@
 
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+
+
+
 function getComputerChoice() {
 
-let computerChoice = Math.floor(Math.random() *3) +1;
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
 
-if (computerChoice === 1){
-    return "Rock"
-} else if (computerChoice === 2){
-    return "Paper"
-} else {
-    return "Scissors"
+    if (computerChoice === 1) {
+        return "Rock"
+    } else if (computerChoice === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+
 }
 
+function getHumanChoice() {
+    let rawHumanChoice = prompt("Please select Rock, Paper, or Scissors!")
+    humanChoice = rawHumanChoice.toLocaleLowerCase()
+
+
 }
 
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+        round++
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!`)
+        humanScore++
+        console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!`)
+        humanScore++
+        console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        humanScore++
+        console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`)
+        computerScore++
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+
+// if (humanChoice != "Rock" || humanChoice != "Paper" || humanChoice != "Scissors") {
+//     alert("Must Choose Rock, Paper, or Scissors!!!")
+//     getHumanChoice()
+
+// }
+
+//  || humanChoice ==="Paper" || humanChoice === "Scissors"
+
+// if(humanChoice !== "Rock"){
+//     alert("Must Choose Rock, Paper, or Scissors!!!")
+//     getHumanChoice()
+// } else if (humanChoice !=="Paper") {
+//     alert("Must Choose Rock, Paper, or Scissors!!!")
+//     getHumanChoice()
+// } else if (humanChoice === "Scissors") {
+//     alert("Must Choose Rock, Paper, or Scissors!!!")
+//     getHumanChoice()
+// }
