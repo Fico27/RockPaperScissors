81eb2934949e8083a018fb00247edbb582675469
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-10 13:20:00 +0100

    Added function getComputerChoice to select three options R, P, or S

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world! Let me love you!")
+
+function getComputerChoice() {
+
+let computerChoice = Math.floor(Math.random() *3) +1;
+
+if (computerChoice === 1){
+    return "Rock"
+} else if (computerChoice === 2){
+    return "Paper"
+} else {
+    return "Scissors"
+}
+
+}
+
