5275da3aef724341d61be58082d0642051f68e9b
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-20 19:48:33 +0100

    Scores are now updated with selections shown.

--- script.js
+++ script.js
@@ -30,12 +30,16 @@ const mainDiv = document.querySelector('div')
 const btnRock = document.querySelector(".btnRock")
 const btnPaper = document.querySelector(".btnPaper")
 const btnScissors = document.querySelector(".btnScissors")
+const guiHumanScore = document.querySelector(".humanScore")
+const guiComputerScore = document.querySelector(".computerScore")
+const action = document.querySelector(".action")
+
 let humanSelection = '';
 
 btnRock.addEventListener("click", () => {
     humanSelection = 'rock';
     const computerSelection = getComputerChoice();
-
+    
     playRound(humanSelection, computerSelection)
 });
 
@@ -68,39 +72,53 @@ btnScissors.addEventListener("click", () => {
 
             if (humanSelection === computerSelection) {
                 console.log("It's a tie!")
+                action.innerHTML = "It's a tie!"
             }
             else if (humanSelection === "rock" && computerSelection === "scissors") {
                 console.log(`You win! ${humanSelection} beats ${computerSelection}!`)
                 humanScore++
                 round++
                 console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+                action.innerHTML = `You win! ${humanSelection} beats ${computerSelection}!`
+                guiHumanScore.innerHTML = humanScore
             }
             else if (humanSelection === "paper" && computerSelection === "rock") {
                 console.log(`You win! ${humanSelection} beats ${computerSelection}!`)
                 humanScore++
                 round++
                 console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+                action.innerHTML = `You win! ${humanSelection} beats ${computerSelection}!`
+                guiHumanScore.innerHTML = humanScore
             }
             else if (humanSelection === "scissors" && computerSelection === "paper") {
                 console.log(`You win! ${humanSelection} beats ${computerSelection}`)
                 humanScore++
                 round++
                 console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+                action.innerHTML = `You win! ${humanSelection} beats ${computerSelection}`
+                guiHumanScore.innerHTML = humanScore
+              
             }
             else {
                 console.log(`You lose! ${computerSelection} beats ${humanSelection}!`)
                 computerScore++
                 round++
                 console.log(`Current score: User: ${humanScore} | Computer: ${computerScore}`)
+                action.innerHTML = `You lose! ${computerSelection} beats ${humanSelection}!`
+                guiComputerScore.innerHTML = computerScore
+         
             }
-
 
             if ((humanScore > computerScore) &&  round === 5){
-                return `You did it! You beat the machine!`
-            } else {
-                return `Humanity is lost... The machine has won!`
+               alert(`You did it! You beat the machine!`)
+            } else if ((computerScore > humanScore) &&  round === 5) {
+                alert(`Humanity is lost... The machine has won!`) 
             }
+
         }
+
+
+
 
         // playRound(humanSelection, computerSelection)
 
